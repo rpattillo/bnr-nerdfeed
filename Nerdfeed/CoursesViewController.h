ff901358c9d9fb8ec3538b8290a662929973fa8a
Ricky Pattillo <ricky.pattillo@gmail.com>
2015-01-17 11:03:27 -0500

    Set up root window
    Creates a navigation controller and sets it as the window's root
    view controller. The navigation view controller's root view controller
    is a minimally stubbed CoursesViewController.

---
+++ Nerdfeed/CoursesViewController.h
@@ -0,0 +1,13 @@
+//
+//  CoursesViewController.h
+//  Nerdfeed
+//
+//  Created by Ricky Pattillo on 1/17/15.
+//  Copyright (c) 2015 Ricky Pattillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoursesViewController : UITableViewController
+
+@end
---
+++ Nerdfeed/CoursesViewController.m
@@ -0,0 +1,27 @@
+//
+//  CoursesViewController.m
+//  Nerdfeed
+//
+//  Created by Ricky Pattillo on 1/17/15.
+//  Copyright (c) 2015 Ricky Pattillo. All rights reserved.
+//
+
+#import "CoursesViewController.h"
+
+@implementation CoursesViewController
+
+#pragma mark - Table View Datasource protocol
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+   return 0;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+   return nil;
+}
+
+
+@end
